94b45565a2be4797c453b53722cb3952d1b5fd38
izelomabarak <joseluisim.lds@gmail.com>
2025-11-01 20:19:54 -0600

    a

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,24 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+
+  // const actualCar = localStorage.getItem("so-cart"); 
+
+  // let newCar = [];
+
+  // if (actualCar) {
+  //   try {
+  //     newCar = JSON.parse(actualCar);
+  //   } catch (error) {
+  //     console.error("Error")
+  //   }
+  // }
+  // cart.push(product);
+
+  // setLocalStorage("so-cart", JSON.stringify(newCar));
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
