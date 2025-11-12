fc1d9294af6c021f943aa859c5471eb9e0b970da
SlayerFluffy <wjezek@byupathway.edu>
2025-11-12 09:55:32 -0700

    submission to continue work from my laptop

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,17 @@
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+    }
+
+    
+};
+
+function productCardTemplate(product) {
+    return 
+};
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const productData = ProductData;
+const products = ProductList;
+console.log(products);
