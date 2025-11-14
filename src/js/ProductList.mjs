8a15c622c7e27c74223a25e59936f4702fa2b7d0
SlayerFluffy <wjezek@byupathway.edu>
2025-11-13 19:27:25 -0700

    individual assignment

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,3 +1,16 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+    return `<li class="product-card">
+    <a href="product_pages/?product=">
+      <img src="" alt="Image of ">
+      <h2 class="card__brand"></h2>
+      <h3 class="card__name"></h3>
+      <p class="product-card__price">$</p>
+    </a>
+  </li>`
+}
+
 export default class ProductList {
     constructor(category, dataSource, listElement) {
         this.category = category;
@@ -7,11 +20,11 @@ export default class ProductList {
 
     async init() {
         const list = await this.dataSource.getData();
+        this.renderList(list);
     }
 
+    renderList(productList) {
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
+    }
     
-};
-
-function productCardTemplate(product) {
-    return 
 };
