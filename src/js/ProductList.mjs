8b932d639558551bee14004411e2a257ad6f0000
amarillafacundo <facundo.e.amarilla@gmail.com>
2025-11-17 01:19:00 -0300

    week2

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,14 +3,14 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
   return `
     <li class="product-card">
-      <a href="product_pages/?products=${product.Id}">
+      <a href="product_pages/index.html?product=${product.Id}">
         <img src="${product.Image}" alt="${product.Name}">
         <h2>${product.Brand.Name}</h2>
         <h3>${product.Name}</h3>
         <p class="product-card__price">$${product.FinalPrice}</p>
       </a>
     </li>
-    `;
+  `;
 }
 
 export default class ProductList{
@@ -25,9 +25,13 @@ export default class ProductList{
         this.renderList(list);
     }
 
-    renderList(){
-        //const htmlStrings = list.map(productCardTemplate);
-        //this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-        renderListWithTemplate(productCardTemplate, this.listElement, list);
-    }
+  renderList(list) {
+    renderListWithTemplate(
+      productCardTemplate,
+      this.listElement,
+      list,
+      "afterbegin",
+      true
+    );
+  }
 }
