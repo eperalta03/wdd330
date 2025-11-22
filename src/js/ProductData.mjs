99e660f6182ac199f04193cfb0828f6801e1c262
amarillafacundo <facundo.e.amarilla@gmail.com>
2025-11-21 23:10:36 -0300

    week3 v1

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -16,7 +16,7 @@ export default class ProductData {
   async getData(category) {
     const response = await fetch(`${baseURL}products/search/${category}`);
     const data = await convertToJson(response);
-    
+
     return data.Result;
   }
   async findProductById(id) {
