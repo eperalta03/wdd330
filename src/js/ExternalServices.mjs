7e19aeb7e95abaf017c660b2bc4691d5c4a8715c
amarillafacundo <facundo.e.amarilla@gmail.com>
2025-11-24 15:00:57 -0300

    update week4 team!

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,28 +1,31 @@
-const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
-async function convertToJson(res) {
-    const data = await res.json();
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
+function convertToJson(res) {
     if (res.ok) {
-        return data;
+        return res.json();
     } else {
-        throw { name: "servicesError", message: data };
+        throw new Error("Bad Response");
     }
 }
 
 export default class ExternalServices {
-    constructor(category) {
+    constructor() {
         // this.category = category;
-        // this.path = `../json/${this.category}.json`;
+        // this.path = `../public/json/${this.category}.json`;
     }
     async getData(category) {
-        const response = await fetch(baseURL + `products/search/${category}`);
+        const response = await fetch(`${baseURL}products/search/${category}`);
         const data = await convertToJson(response);
+
         return data.Result;
     }
     async findProductById(id) {
-        const response = await fetch(baseURL + `product/${id}`);
+        const response = await fetch(`${baseURL}product/${id}`);
         const data = await convertToJson(response);
+        // console.log(data.Result);
         return data.Result;
     }
+
     async checkout(payload) {
         const options = {
             method: "POST",
@@ -31,6 +34,6 @@ export default class ExternalServices {
             },
             body: JSON.stringify(payload),
         };
-        return await fetch(baseURL + "checkout/", options).then(convertToJson);
+        return await fetch(`${baseURL}checkout/`, options).then(convertToJson);
     }
 }
