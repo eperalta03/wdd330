59aea0125abd692d78c6a2341f308aa15f26b3de
amarillafacundo <facundo.e.amarilla@gmail.com>
2025-12-01 17:49:17 -0300

    update week 4

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -6,7 +6,7 @@ function convertToJson(res) {
   }
 }
 
-export default class ProductData {
+export default class ExternalServices {
   constructor(category) {
     this.category = category;
     this.path = `../public/json/${this.category}.json`;
